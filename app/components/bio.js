2d1e906798540c56c44e9393fdc0ff6b72e6d051
mattpocock <mattpocockvoice@gmail.com>
2018-01-04 17:57:51 +0000

    Bio Edits

--- app/components/bio.js
+++ app/components/bio.js
@@ -10,14 +10,12 @@ export class Bio extends React.Component {
 
                 <div className="col-xs-10 col-sm-9 col-md-5 bio__text-container">
                     <h3>About Me</h3>
-                    <p>My name's Matt. I'm a conscientious, user-focused developer with exceptional communication skills.
+                    <p>My name's Matt. I'm a conscientious, user-focused developer with exceptional communication.
                         I've got skills in Javascript, jQuery, SASS, HTML5 and Wordpress, and I'm looking for a full-time role at a junior level.</p>
                     <p>Before I became a developer, I taught over 4,000 lessons as a voice coach: public speaking, accents and communication.</p>
                     <p>I've worked with clients from the BBC, NHS, local Government, ITV, Sky, Time Warner - among many others.
                     I shared a stage with Channel 4's Jon Snow at Accenture's annual conference to 2,000 people.</p>
                     <p>I hold a Masters in Voice (Distinction) from Guildford School of Acting, and a 1:1 Drama BA from Exeter University.</p>
-                    <p>I found joy in teaching from communicating difficult ideas in simple terms,
-                        and good design always starts with good communication.</p>
                     <p>I'm looking forward to bringing my experience to bear on web development,
                         and I hope we can work together.
                         </p>
